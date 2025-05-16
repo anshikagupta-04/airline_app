09f133b8db815f5ae63f0aa7375ea134a5ac2f7b
anshikagupta-04 <pg8998211@gmail.com>
2025-05-16 21:51:53 +0530

    h2 file based database

--- src/main/java/com/example/airline/controller/ScheduleController.java
+++ src/main/java/com/example/airline/controller/ScheduleController.java
@@ -1,19 +1,15 @@
 package com.example.airline.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.*;
-import java.time.LocalDate;
 import com.example.airline.model.*;
 import com.example.airline.repository.*;
 
 @RestController
 @RequestMapping("/schedule")
 public class ScheduleController {
-    @Autowired private FlightRepository flightRepo;
     @Autowired private ScheduleRepository scheduleRepo;
     
     @PostMapping()
